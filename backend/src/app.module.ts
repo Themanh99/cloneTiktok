ded2314df0ab4ff377b8bea3c7cb8a83033838e3
manhct <manhct@gmail.com>
2026-04-22 22:34:17 +0700

    add setting prettier

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,24 +8,23 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-  ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: '.env',
-  }), 
-  
-  // 2. Throttler Module
-  // ttl: 60000ms = 1 phut
-  // limit: 100 request per minute per IP / 1 phut
+    // 1. Config Module cần đặt ở đầu để load trước , nếu đặt sau có thằng nào dùng sẽ bị crash
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
 
-  ThrottlerModule.forRoot([
-    {ttl: 60000, limit: 100},
-  ]),
+    // 2. Throttler Module
+    // ttl: 60000ms = 1 phut
+    // limit: 100 request per minute per IP / 1 phut
 
-  // 3. Schedule Module
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]),
 
-  ScheduleModule.forRoot(),
-  UserModule
-],
+    // 3. Schedule Module
+
+    ScheduleModule.forRoot(),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
